698325be656b71b2587a3262c71324d7422b6acd
tingfeng-key <1770963469@qq.com>
2022-10-03 01:34:49 +0800

    优化

--- lib/grammers-client/src/types/login_token.rs
+++ lib/grammers-client/src/types/login_token.rs
@@ -6,7 +6,24 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#[derive(Debug, Clone)]
 pub struct LoginToken {
     pub(crate) phone: String,
     pub(crate) phone_code_hash: String,
+}
+
+impl LoginToken {
+    pub fn build(phone: String, phone_code_hash: String) -> Self {
+        Self {
+            phone,
+            phone_code_hash,
+        }
+    }
+    pub fn phone(&self) -> &str {
+        self.phone.as_str()
+    }
+
+    pub fn phone_code_hash(&self) -> &str {
+        self.phone_code_hash.as_ref()
+    }
 }
