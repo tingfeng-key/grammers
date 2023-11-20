43b56ef0886813a12a42f84bd2be8512aa5134b2
tingfeng-key <1770963469@qq.com>
2023-11-20 18:29:55 +0800

    修复bug

--- lib/grammers-client/src/types/input_peer.rs
+++ lib/grammers-client/src/types/input_peer.rs
@@ -2,14 +2,14 @@ use grammers_tl_types as tl;
 pub struct InputPeer(pub(crate) tl::enums::InputPeer);
 
 impl InputPeer {
-    pub fn from(raw: tl::enums::InputPeer) -> Self {
+    pub fn _from_raw(raw: tl::enums::InputPeer) -> Self {
         Self(raw)
     }
 }
 
 #[cfg(feature = "unstable_raw")]
-impl From<InputUser> for tl::enums::InputUser {
-    fn from(input_user: InputUser) -> Self {
+impl From<InputPeer> for tl::enums::InputPeer {
+    fn from(input_user: InputPeer) -> Self {
         input_user.0
     }
 }
