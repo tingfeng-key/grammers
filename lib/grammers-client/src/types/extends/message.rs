aaef050a5bc651cbc5ec95ed2bd21e713ff14451
tingfeng-key <1770963469@qq.com>
2024-08-28 23:28:00 +0800

    merge_and_update

--- lib/grammers-client/src/types/extends/message.rs
+++ lib/grammers-client/src/types/extends/message.rs
@@ -1,10 +1,10 @@
 use crate::types;
 
-pub fn fmt_entities_new(msg: types::Message) -> Vec<super::entity::Entity> {
+pub fn fmt_entities_new(msg: &types::Message) -> Vec<super::entity::Entity> {
     super::entity::Entity::_from_message(msg.text(), msg.fmt_entities().map(|x| x.clone()))
 }
 
-pub fn parse_usernames_from_entities(msg: types::Message) -> Vec<String> {
+pub fn parse_usernames_from_entities(msg: &types::Message) -> Vec<String> {
     // println!("origin entities:{:#?}", self.fmt_entities());
     let entities: Vec<String> = fmt_entities_new(msg)
         .iter()
